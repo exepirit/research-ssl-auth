63b5cb490f302172d9083c04e3c7106595ad66f6
exepirit <exepir1t@protonmail.com>
2024-04-02 12:00:36 +0700

    feat: get user from context

--- go/internal/middleware/ssl_auth.go
+++ go/internal/middleware/ssl_auth.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 )
 
+const ctxUserKey = "user"
+
 // NewAuthMiddleware создает новую аутентифицирующую middleware.
 func NewAuthMiddleware(handler http.Handler, authenticator authn.Authenticator) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -15,8 +17,18 @@ func NewAuthMiddleware(handler http.Handler, authenticator authn.Authenticator) 
 			return
 		}
 
-		ctx := context.WithValue(r.Context(), "user", user)
+		ctx := context.WithValue(r.Context(), ctxUserKey, user)
 		r = r.WithContext(ctx)
 		handler.ServeHTTP(w, r)
 	})
+}
+
+// GetCurrentUser извлекает текущего пользователя, указанного в контексте.
+func GetCurrentUser(ctx context.Context) (authn.User, bool) {
+	value := ctx.Value(ctxUserKey)
+	if value == nil {
+		return nil, false
+	}
+	user, ok := value.(authn.User)
+	return user, ok
 }
